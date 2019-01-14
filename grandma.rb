1d1ee6742ffd86cea23147856c9fb9442f0dec1a
Salman Ahmad <salman.neim@gmail.com>
2019-01-14 07:30:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return ""
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif string.uppercase
   return "NO, NOT SINCE 1938!"
 end
