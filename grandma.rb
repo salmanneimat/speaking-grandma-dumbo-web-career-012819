ee435ab0b8b10b43eafffd10db0870c0a87d643a
Salman Ahmad <salman.neim@gmail.com>
2019-01-14 07:32:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,6 +21,9 @@ def speak_to_grandma(phrase)
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
+      else
+  		return "NO, NOT SINCE 1938!"
+    end
 end
 
 end
